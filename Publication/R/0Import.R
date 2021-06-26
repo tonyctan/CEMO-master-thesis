3749c6dfbc7d89906b3121c267d7300a9fbd207f
Tony Tan <tony.c.tan@gmail.com>
2021-06-26 23:57:35 +0200

    Prepare dataset into Mplus-ready format

--- Publication/R/0Import.R
+++ Publication/R/0Import.R
@@ -113,14 +113,16 @@ for (j in 1:dim(CNT_dummy)[2]) {
 finlit <- cbind(CNT_dummy, finlit)
 rm(CNT_dummy) # Keep object list clean
 # Re-order the columns so that country dummies follows country code order
-finlit <- finlit[, c(2,1,3,4,6,7,8,9,10,12,11,13,14,15,16,17,18,19,5,20,21:68)]
+# Also, drop CNT column (Column 21)
+finlit <- finlit[, c(2,1,3,4,6,7,8,9,10,12,11,13,14,15,16,17,18,19,5,20,22:62,64,63,65:67)]
 
+# Turn texts "0" and "1" to numeric 0 and 1 respectively
 finlit[,c(1:20)] <- lapply(finlit[,c(1:20)], as.numeric)
 
 # Use data.table for better RAM management
 library(data.table); setDTthreads(0) # 0 means all the available cores
-# Export data into a CSV file for faster import next time
+# Export data to an Mplus-ready format
 fwrite(finlit,
-    file = "pub.csv",
-    na = "NA", row.names = F, col.names = T
+    file = "pub.dat",
+    na = "-99", row.names = F, col.names = F
 )
